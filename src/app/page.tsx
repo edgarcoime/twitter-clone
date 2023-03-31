949888659e7898124120c7d5144949a34d0e808d
Edgar Coime <59620774+edgarcoime@users.noreply.github.com>
2023-03-31 18:41:45 +0000

    feat: basic follow bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/navigation/Header";
+
 export default function Home() {
   return (
-    <div className='text-3xl text-sky-500'>
-      Hello world!
-    </div>
+    <>
+      <Header label="Home" />
+    </>
   )
 }
